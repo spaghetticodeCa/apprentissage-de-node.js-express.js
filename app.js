b29e288609751b0b101f2da1665d466b7c09cc11
SpaghettiCodeCa <tommyaudet@spaghetticode.ca>
2022-11-17 12:24:41 -0500

    ajout de model et schema pour faire des requetes a la database grace a mongoose, display des post dans mes views

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
 
 const app = express();
+
+const dbURI = 'mongodb+srv://tommy:live4cob@cluster0.wh9bb.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => {
+    app.listen(3000);
+    console.log('connected to db');
+  })
+  .catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -10,21 +21,14 @@ app.set('view engine', 'ejs');
 // register
 
 //listen for requests
-app.listen(3000);
 
 app.use(express.static('public'));
 
 app.use(morgan('dev'));
 
+
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yoshi find eggs', snippet: 'alsd;fkjjsaflkasf laksdjfsalkdfj asdflkjasdlkfj alsdkfjsadlk'},
-    {title: 'Yoshi find bitches', snippet: 'alsd;fkjjsaflkasf laksdjfsalkdfj asdflkjasdlkfj alsdkfjsadlk'},
-    {title: 'Yoshi find some shits', snippet: 'alsd;fkjjsaflkasf laksdjfsalkdfj asdflkjasdlkfj alsdkfjsadlk'},
-  ];
-
-  res.render('index', {title: 'home', blogs});
-
+  res.redirect('/blogs');
 });
 app.get('/about', (req, res) => {
   //res.send('<p>about page</p>');
@@ -32,6 +36,15 @@ app.get('/about', (req, res) => {
 
 });
 
+
+
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1})
+    .then((result) => {
+      res.render('index', {title: 'All Blogs', blogs: result})
+    })
+    .catch((err) => {console.log(err);});
+});
 app.get('/blogs/create', (req, res) => {
   res.render('create', {title: 'create a new blog'});
 });
