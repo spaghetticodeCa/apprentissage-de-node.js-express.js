cfa57c81f4afda9551f95576259d351d8dff312a
SpaghettiCodeCa <tommyaudet@spaghetticode.ca>
2022-11-17 11:17:03 -0500

    ajout de middleware qui sert une static file css et ajout de morgan qui log les activites sur le serveur

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -11,19 +12,10 @@ app.set('view engine', 'ejs');
 //listen for requests
 app.listen(3000);
 
+app.use(express.static('public'));
 
-app.use((req, res, next) => {
-  console.log('new request made:');
-  console.log('host :', req.hostname);
-  console.log('path :', req.path);
-  console.log('method :', req.method);
-  next();
-});
+app.use(morgan('dev'));
 
-app.use((req, res, next) => {
-  console.log('in the next middleware');
-  next();
-});
 app.get('/', (req, res) => {
   const blogs = [
     {title: 'Yoshi find eggs', snippet: 'alsd;fkjjsaflkasf laksdjfsalkdfj asdflkjasdlkfj alsdkfjsadlk'},
