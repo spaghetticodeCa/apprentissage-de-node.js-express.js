527f9a1ceaf7fbe05892abc415fff16c858d7f9f
SpaghettiCodeCa <tommyaudet@spaghetticode.ca>
2022-11-20 22:55:18 -0500

    ajout de suppression de post dans detail page et redirection vers le home page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
+const { render } = require('ejs');
 
 
 const app = express();
@@ -23,7 +24,8 @@ app.set('view engine', 'ejs');
 //listen for requests
 
 app.use(express.static('public'));
-
+//to accecpt form data
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 
@@ -45,9 +47,45 @@ app.get('/blogs', (req, res) => {
     })
     .catch((err) => {console.log(err);});
 });
+
 app.get('/blogs/create', (req, res) => {
   res.render('create', {title: 'create a new blog'});
 });
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+  blog.save()
+   .then((result) => {
+     // rediriger vers blogs pour qu on voit la liste de blogs apres le post
+    res.redirect('/blogs');
+   })
+   .catch((err) => {
+     console.log(err);
+    });
+});
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' })
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render('details', { blog: result, title: 'Blog Details' })
+    })
+    .catch((err) => {console.log(err);});
+});
+
+
 
 // must stay bottom of the page
 app.use((req, res) => {
